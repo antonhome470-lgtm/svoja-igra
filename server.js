bb7166fe00cc25772f7a747ade062b2e00eb9adf
antonhome470-lgtm <antonhome470@gmail.com>
2026-03-27 19:41:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -602,14 +602,6 @@ io.on('connection', (socket) => {
 
     console.log(`${data.name} → ${data.roomId}`);
 
-    if (room.autoHost) {
-      io.to(data.roomId).emit('auto-waiting', {
-        message: 'Нажмите "Начать игру", когда все подключатся',
-        playerCount: room.players.size
-      });
-    }
-  });
-
   // --- Ручной старт авто-игры ---
   socket.on('auto-start-now', () => {
     const room = getRoom();
